459cc63e9bfef6d131a75edd1ae5fc1ce8013b3e
2020136178 <rhrnak0708@g.shingu.ac.kr>
2022-04-10 21:21:19 +0900

    홈화면 수정

--- app/src/main/java/com/example/kkanbu/MainActivity.java
+++ app/src/main/java/com/example/kkanbu/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.kkanbu;
 
+import static com.example.kkanbu.R.drawable.btn_monitoring3;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
