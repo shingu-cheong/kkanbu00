29a2dca7a185e6dadab859950417d5957297890e
2020136178 <rhrnak0708@gmail.com>
2022-09-20 16:03:20 +0900

    firebase 구글 로그인 연동중

--- app/src/main/java/com/example/kkanbu/LoginActivity.java
+++ app/src/main/java/com/example/kkanbu/LoginActivity.java
@@ -1,14 +1,96 @@
 package com.example.kkanbu;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button google_login;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        google_login=findViewById(R.id.google_login);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+        google_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+
     }
+
+    private void SignIn() {
+
+        Intent intent =gsc.getSignInIntent();
+//        startActivityForResult(intent,100);
+        mStartForResult.launch(intent);
+
+    }
+
+
+//    ActivityResultLauncher<Intent>  resultLauncher = registerForActivityResult(
+//            new ActivityResultContracts.StartActivityForResult(),
+//            result -> {
+//                //result.getResultCode()를 통하여 결과값 확인
+//                if(result.getResultCode() == RESULT_OK) {
+//                    //ToDo
+//                }
+//                if(result.getResultCode() == RESULT_CANCELED){
+//                    //ToDo
+//                }
+//            }
+//    });
+//
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                //result.getResultCode()를 통하여 결과값 확인
+                if(result.getResultCode() == RESULT_OK) {
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                    try{
+                        task.getResult(ApiException.class);
+                    } catch (ApiException e) {
+                        Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show();
+                        finish();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                        startActivity(intent);
+                    }
+                }
+                if(result.getResultCode() == RESULT_CANCELED){
+                    //ToDo
+                }
+            }
+    );
+
+
 }
---
+++ app/src/main/java/com/example/kkanbu/usersetting/LoginActivity2.java
@@ -0,0 +1,7 @@
+package com.example.kkanbu.usersetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+}
